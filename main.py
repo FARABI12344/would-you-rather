6866b4f793555b0aeee01db9c1d4eeccaaf0177b
FARABI12344 <farabiehan4@gmail.com>
2025-03-20 22:08:48 +0600

    Update and rename test to main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Run the app on localhost port 5000
+    app.run(debug=True)
+
